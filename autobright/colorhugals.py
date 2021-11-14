66e5c643805774bfb414a769b278b00d0995d636
Martin Ueding <mu@martin-ueding.de>
2021-11-14 12:36:40 +0100

    Copy colorhug code

---
+++ autobright/colorhugals.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright © 2016 Martin Ueding <martin-ueding.de>
+# Licensed under The MIT License
+import subprocess
+
+from autobright.core import Sensor
+
+
+class ColorHug(Sensor):
+    def get_reading(self) -> float:
+
+        command = ["colorhug-cmd", "set-multiplier", "20"]
+        subprocess.check_call(command)
+
+        command = ["colorhug-cmd", "set-color-select", "white"]
+        subprocess.check_call(command)
+
+        command = ["colorhug-cmd", "take-reading-raw"]
+        output = subprocess.check_output(command).decode().strip()
+
+        command = ["colorhug-cmd", "set-multiplier", "0"]
+        subprocess.check_call(command)
+
+        words = output.split()
+        reading = float(words[1])
+        return reading
