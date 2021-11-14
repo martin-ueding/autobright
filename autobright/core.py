e09d9fc4661e5199e2b0c4faec381bddc5d7dd97
Martin Ueding <mu@martin-ueding.de>
2021-11-14 12:40:47 +0100

    Whitespace

--- autobright/core.py
+++ autobright/core.py
@@ -1,14 +1,17 @@
 import abc
 
+
 class Sensor(abc.ABC):
     @abc.abstractmethod
     def get_reading(self) -> float:
         raise NotImplementedError()
 
+
 class Mapping(abc.ABC):
     @abc.abstractmethod
     def map(self, reading: float) -> float:
         raise NotImplementedError()
+
 
 class Display(abc.ABC):
     @abc.abstractmethod
