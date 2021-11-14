39fc8b85eee645998a429cc1001409cb687b0d3e
Martin Ueding <mu@martin-ueding.de>
2021-11-14 12:39:53 +0100

    Move DDCControl over

---
+++ autobright/ddccontrol.py
@@ -0,0 +1,25 @@
+import subprocess
+
+from autobright.core import Display
+
+
+class DDCControl(Display):
+    def __init__(self, device: int):
+        self.device = device
+
+    def set_brightness(self, percent: float) -> None:
+        brightness = int(percent * 100)
+        assert 0 <= brightness <= 100, "Brightness must be between 0 and 100."
+
+        command = [
+            "ddccontrol",
+            "-r",
+            "0x10",
+            "-w",
+            str(brightness),
+            f"dev:/dev/i2c-{self.device}",
+        ]
+        status = subprocess.call(command)
+
+        if status != 0:
+            print("ddccontrol failed but it might still have worked")
