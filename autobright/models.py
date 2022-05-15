9a4bbb0b300e308d694391b8345e9c01220127d8
Martin Ueding <mu@martin-ueding.de>
2022-05-15 16:56:13 +0200

    Add spline model

--- autobright/models.py
+++ autobright/models.py
@@ -1,3 +1,7 @@
+from typing import List
+
+import scipy.interpolate
+
 from autobright.core import BrightnessModel
 
 
@@ -5,3 +9,11 @@ class ProportionalBrightnessModel(BrightnessModel):
     def map(self, reading: int) -> int:
         b = 0.0211292
         return int(min(b * reading, 100))
+
+
+class SplineModel(BrightnessModel):
+    def __init__(self, base_x: List[int], base_y: List[int]):
+        self.interpolator = scipy.interpolate.interp1d(base_x, base_y, kind="quadratic")
+
+    def map(self, reading: int) -> int:
+        return self.interpolator(reading)
